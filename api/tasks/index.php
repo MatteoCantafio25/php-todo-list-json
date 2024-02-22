a2533e08a8b8d3f2e3cba912b9591db61409cbc8
MatteoCantafio25 <mattyteo004@gmail.com>
2024-02-22 19:35:34 +0100

    Exercise completed

---
+++ api/tasks/index.php
@@ -0,0 +1,40 @@
+<?php
+// Dico che voglio rispondere in linguaggio json
+header('Content-Type: application/json');
+
+
+// Salvo il path del file json
+$source_path = __DIR__ . '/../../database/tasks.json';
+
+// Prendo i contenuti del file json
+$json_data = file_get_contents($source_path);
+
+// Converto i dati json un array associativo php
+$tasks = json_decode($json_data, true);
+
+// Controllo se ho un nuovo task
+$new_task_name = $_POST['task'] ?? '';
+
+
+// Controllo se ho un nuovo task
+if(!empty($new_task_name)){
+
+    // Creo un nuovo tast sotto forma di oggetto
+    $new_task = new stdClass();
+    $new_task->id = count($tasks) + 1;
+    $new_task->text = $new_task_name;
+    $new_task->done = false;
+
+    // Aggiungo il new_task all'array tasks
+    $tasks[] = $new_task;  
+    
+    // RRiconverto l'array di task in json
+    $tasks_json = json_encode($tasks);
+
+    //Sovrascrivo l'array originale
+    file_put_contents($source_path, $tasks_json);
+} 
+
+// Stampo i task, riconvertendoli in json
+    echo json_encode($tasks);
+?>
--- my_app/js/script.js
+++ my_app/js/script.js
@@ -1,10 +1,32 @@
 console.log("Vue ok", Vue);
 
+const endpoint = "http://localhost/boolean/php-todo-list-json/api/tasks/";
+
 const { createApp } = Vue;
 
 const app = createApp({
   name: "PHP ToDo List JSON",
-  data() {},
+  data: () => ({
+    tasks: [],
+    newTask: "",
+  }),
+  methods: {
+    addTask() {
+      const data = { task: this.newTask };
+
+      const config = { headers: { "Content-Type": "multipart/form-data" } };
+
+      axios.post(endpoint, data, config).then((res) => {
+        this.tasks = res.data;
+        this.newTask = "";
+      });
+    },
+  },
+  created() {
+    axios.get(endpoint).then((res) => {
+      this.tasks = res.data;
+    });
+  },
 });
 
 app.mount("#root");
